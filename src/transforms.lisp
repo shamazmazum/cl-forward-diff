ed3147b542d69797a7f6123d9ba9e0a4459e5ff0
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-10-03 08:48:55 +0300

    More transforms

--- src/transforms.lisp
+++ src/transforms.lisp
@@ -20,6 +20,10 @@
 (sb-c:deftransform two-arg-* ((x y) (real dual) cl:*)
   '(simd:f64.2* (fill-dual-vector x) y))
 
+;; Real types are optimized by SBCL.
+;; Thx to all gods, that DUAL is the most contagious.
+;; NB: These optimizations cannot be done for EXT-NUMBER, because we
+;; don't know if Y is more contagious than X.
 (sb-c:deftransform two-arg-*
     ((x y) (dual (sb-int:constant-arg (member 2 2.0 2d0))) cl:*)
   '(+ x x))
@@ -42,9 +46,44 @@
 (sb-c:deftransform two-arg-/ ((x y) (real dual) cl:*)
   '(dual-dual-/ (make-dual (float x 0d0)) y))
 
+;; +/-/*// with identity
+;; Again, only for DUAL type, as SBCL makes the same for REAL
+(macrolet ((def (op identity)
+             `(sb-c:deftransform ,op
+                  ((x y) (dual (sb-int:constant-arg real)) cl:*
+                   ;; Beware the SNaN and negative zeros
+                   :policy (zerop sb-c::float-accuracy))
+                (if (= (sb-c:lvar-value y) ,identity)
+                'x (sb-c::give-up-ir1-transform)))))
+  (def two-arg-+ 0)
+  (def two-arg-- 0)
+  (def two-arg-* 1)
+  (def two-arg-/ 1))
+
+;; Multiplication by zero
+(sb-c:deftransform two-arg-*
+    ((x y) (dual (sb-int:constant-arg (member 0 0.0 0d0))) cl:*
+                 ;; Beware the SNaN
+                 :policy (zerop sb-c::float-accuracy))
+  '(make-dual 0d0 0d0))
+
 ;; expt
 (sb-c:deftransform expt ((base power) (dual real) cl:*)
   '(dual-expt base power))
 
 (sb-c:deftransform expt ((base power) (real real) cl:*)
   '(real-expt base power))
+
+(sb-c:deftransform expt
+    ((base power) (t (sb-int:constant-arg real)) cl:*)
+  (let ((val (sb-c:lvar-value power)))
+    (cond
+      ;; I skip (= power 0) because I don't know the type of 1 I
+      ;; should return.
+      ((= val +1) 'base)
+      ((= val -1) '(/ base))
+      ((= val +2) '(* base base))
+      ((= val -2) '(/ (* base base)))
+      ((= val +3) '(* base base base))
+      ((= val -3) '(/ (* base base base)))
+      (t (sb-c::give-up-ir1-transform)))))
